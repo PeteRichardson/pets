c4818bf4f9c5fdc8243a1f1451308720ef945f18
Pete Richardson <pete@peterichardson.com>
2018-01-11 22:24:19 -0800

    added simple unit tests for Pet, Cat, Dog

--- pets.py
+++ pets.py
@@ -1,27 +1,41 @@
-class Pet:
-	def __init__(self, name, species, age, breed, owner=None):
-		self.name = name
-		self.species = species
-		self.age = age
-		self.breed = breed
-		self.owner = owner
-		self.sound = ""
+from abc import ABC, abstractmethod
 
-	def speak(self):
-		print("{} the {} says '{}!'".format(self.name, self.species.lower(), self.sound))
+class Pet(ABC):
+    def __init__(self, name, species, age, breed):
+        self.name = name
+        self.species = species
+        self.age = age
+        self.breed = breed      
+
+        if not 0 < self.age < 25:
+            raise ValueError("Invalid age... {}. Expected 0 < age < 25".format(self.age))
+
+    @property
+    @abstractmethod
+    def sound(self):
+        pass
+
+    def speak(self):
+        print("{} the {} says '{}!'".format(self.name, self.species.lower(), self.sound))
 
 class Cat (Pet):
-	def __init__(self, name, age, breed, owner=None):
-		super().__init__(name, "Cat", age, breed, owner)
-		self.sound = "Meow"
+    def __init__(self, name, age, breed):
+        super().__init__(name, "Cat", age, breed)
+    
+    @property
+    def sound(self):
+        return "Meow"
 
 class Dog (Pet):
-	def __init__(self, name, age, breed, owner=None):
-		super().__init__(name, "Dog", age, breed, owner)
-		self.sound = "Woof"
+    def __init__(self, name, age, breed):
+        super().__init__(name, "Dog", age, breed)
+    
+    @property
+    def sound(self):
+       return "Woof"
 
 if __name__ == "__main__":
-	bella = Cat("Bella", 3, "American Shorthair", None)
-	bella.speak()
-	silly = Dog("Silly", 4, "Australian Shepherd", None)
-	silly.speak()
+    bella = Cat("Bella", 3, "American Shorthair")
+    bella.speak()
+    silly = Dog("Silly", 4, "Australian Shepherd")
+    silly.speak()
---
+++ test_pets.py
@@ -0,0 +1,37 @@
+import unittest
+
+from pets import Pet, Cat, Dog
+
+class TestPet(unittest.TestCase):
+	def test_cant_instantiate_pet(self):
+		""" Pet is abstract and cant be instantiated """
+		with self.assertRaises(TypeError):
+			pet = Pet("Waldo", "Gerbil", 8, "Mongolian Gerbil")
+
+class TestCat (unittest.TestCase):
+	def setUp(self):
+		self.samplecat = Cat("Bella", 3, "American Shorthair")
+
+	def test_catsound(self):
+		""" Cats should Meow """
+		self.assertEqual(self.samplecat.sound, "Meow")
+
+	def test_age_out_of_bounds(self):
+		""" Ages outside of 0-25 should throw """
+		with self.assertRaises(ValueError):
+			unborncat = Cat("Fetus", 0, "Main Coon")
+
+		with self.assertRaises(ValueError):
+			oldcat = Cat("Methuselah", 40, "Egyptian Sphinx")
+
+class TestDog(unittest.TestCase):
+	def setUp(self):
+		self.sampledog = Dog("Silly", 4, "Austrailan Shepherd")
+
+	def test_dogsound(self):
+		""" Dogs should Woof """
+		self.assertEqual(self.sampledog.sound, "Woof")
+
+
+if __name__ == "__main__":
+	unittest.main()
