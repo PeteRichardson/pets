3e3a8f6f053db95faa400ca96793c123e91965c4
Pete Richardson <peterichardson@apple.com>
2018-01-27 16:29:34 -0800

    almost autoload .py files in pets dir as Pet subclasses
    Still need to replace individual _import_one calls with a loop over _list_pet_classes()

--- pets/__init__.py
+++ pets/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import importlib
+import sys
 
 def _list_pet_classes():
     from os.path import dirname, basename, isfile
@@ -8,50 +9,20 @@ def _list_pet_classes():
     pyfiles = glob.glob(dirname(__file__)+"/*.py")
     classnames = [ basename(f)[:-3] for f in pyfiles if isfile(f) and not f.endswith('__init__.py')]
     classnames.remove("pet")
-    #return ["pet"] + classnames   # make sure Pet class is imported first
-    return ["pet","horse"]
+    return ["pet"] + classnames   # make sure Pet class is imported first
 
-def import_one(classname):
-    if classname == 'pet':
-        print "importing {}".format(classname)
-        submodule = "pets.{}".format(classname)
-        print "submodule = {}".format(submodule)
-        m = importlib.import_module(submodule,classname)
-        print "m = {}".format(m)
-        actualpetclass = getattr(m,classname.capitalize())
-        print "actualpetclass = {}".format(actualpetclass)
-        setattr(module, classname.capitalize(), actualpetclass)
-        print dir(module)
-    else:
-        print "importing {}".format(classname)
-        submodule = "pets.{}".format(classname)
-        print "submodule = {}".format(submodule)
-        m = importlib.import_module(submodule,classname)
-        print "m = {}".format(m)
-        actualpetclass = getattr(m,classname.capitalize())
-        print "actualpetclass = {}".format(actualpetclass)
-        setattr(module, classname.capitalize(), actualpetclass)
-        print dir(module)
- 
-import sys
+def _import_one(classname):
+    module = sys.modules[__name__]
+    submodule = importlib.import_module("pets.{}".format(classname))
+    clas = getattr(submodule, classname.capitalize())
+    setattr(module, classname.capitalize(), clas)
+    module.__dict__.pop(classname,None)
+    return clas
 
-module = sys.modules[__name__]  # pets module
-submodule = importlib.import_module("pets.pet", "pet") # pets.pet module
-clas = getattr(submodule, "Pet")  # pets.pet.Pet class
-setattr(module, "Pet", clas)
-Pet = clas
+Pet   = _import_one("pet")
+Cat   = _import_one("cat")
+Dog   = _import_one("dog")
+Horse = _import_one("horse")
 
-submodule = importlib.import_module("pets.cat") # pets.pet module
-clas = getattr(submodule, "Cat")  # pets.pet.Pet class
-setattr(module, "Cat", clas)
-Cat = clas
-
-submodule = importlib.import_module("pets.dog") # pets.pet module
-clas = getattr(submodule, "Dog")  # pets.pet.Pet class
-setattr(module, "Dog", clas)
-Dog = clas
-
-submodule = importlib.import_module("pets.horse") # pets.pet module
-clas = getattr(submodule, "Horse")  # pets.pet.Pet class
-setattr(module, "Horse", clas)
-Horse = clas
+del sys
+del importlib
