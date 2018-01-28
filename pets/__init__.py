344de73958a4e3996f2673301af3a48974c6936a
Pete Richardson <peterichardson@apple.com>
2018-01-27 16:51:45 -0800

    Replace import calls with a loop

--- pets/__init__.py
+++ pets/__init__.py
@@ -7,22 +7,21 @@ def _list_pet_classes():
     from os.path import dirname, basename, isfile
     import glob
     pyfiles = glob.glob(dirname(__file__)+"/*.py")
-    classnames = [ basename(f)[:-3] for f in pyfiles if isfile(f) and not f.endswith('__init__.py')]
-    classnames.remove("pet")
-    return ["pet"] + classnames   # make sure Pet class is imported first
+    classnames = [ basename(f)[:-3].capitalize() for f in pyfiles if isfile(f) and not f.endswith('__init__.py')]
+    classnames.remove("Pet")
+    return ["Pet"] + classnames   # make sure Pet class is imported first
 
 def _import_one(classname):
     module = sys.modules[__name__]
-    submodule = importlib.import_module("pets.{}".format(classname))
-    clas = getattr(submodule, classname.capitalize())
-    setattr(module, classname.capitalize(), clas)
-    module.__dict__.pop(classname,None)
+    submodule = importlib.import_module("pets.{}".format(classname.lower()))
+    clas = getattr(submodule, classname)
+    setattr(module, classname, clas)
+    module.__dict__.pop(classname.lower(),None)
     return clas
 
-Pet   = _import_one("pet")
-Cat   = _import_one("cat")
-Dog   = _import_one("dog")
-Horse = _import_one("horse")
+for classname in _list_pet_classes():
+    setattr(sys.modules[__name__], classname, _import_one(classname))
 
 del sys
 del importlib
+del classname
