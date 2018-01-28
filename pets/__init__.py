2397279d105c38d9c689401cbec34cd9203391b5
Pete Richardson <peterichardson@apple.com>
2018-01-27 16:58:07 -0800

    Simplify the import loop and remove some code

--- pets/__init__.py
+++ pets/__init__.py
@@ -12,15 +12,17 @@ def _list_pet_classes():
     return ["Pet"] + classnames   # make sure Pet class is imported first
 
 def _import_one(classname):
-    module = sys.modules[__name__]
     submodule = importlib.import_module("pets.{}".format(classname.lower()))
     clas = getattr(submodule, classname)
+    module = sys.modules[__name__]
     setattr(module, classname, clas)
     module.__dict__.pop(classname.lower(),None)
+    #setattr(sys.modules[__name__], classname, clas)
+    #sys.modules[__name__].__dict__.pop(classname.lower(),None)
     return clas
 
 for classname in _list_pet_classes():
-    setattr(sys.modules[__name__], classname, _import_one(classname))
+    _import_one(classname)
 
 del sys
 del importlib
