0741e78be5b777bd962f0d49ad1c3a8ffb85282a
Pete Richardson <pete@peterichardson.com>
2018-11-08 13:11:40 -0800

    Add Snake class.   Set CORS headers

---
+++ pets/snake.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+from pets import Pet
+
+class Snake (Pet):
+    breeds = [  "King Cobra",
+                "American Copperhead",
+                "Black Mamba",
+                "Corn Snake",
+                "Rattlesnake",
+                "Boa Constrictor",
+                "Eastern Coral Snake",
+                "Black Rat Snake",
+                "Burmese Python",
+                "Ball Python",
+                "Royal Python",
+                "Reticulated Python",
+                "Garter Snake",
+                "Green Anaconda",
+                "Water Moccasin Snake",
+                "Green Tree Python"]
+    
+    species = "Snake"
+
+    def __init__(self, name=None, age=None, breed=None):
+        super(Snake, self).__init__(Snake.species, name=name, age=age, breed=breed)
+    
+    @property
+    def sound(self):
+       return "Hisss"
+
+if __name__ == "__main__":
+    striker = Snake("Striker", 6, "Rattlesnake")
+    striker.speak()
